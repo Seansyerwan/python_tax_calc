1dc456f1b27bad8d7cc4dd59d45cd33ddf9daa06
Seansyerwan <Seankerrigan122016@gmail.com>
2024-07-08 17:32:54 +0100

    expanded upon prsi function

--- prsi_func.py
+++ prsi_func.py
@@ -2,9 +2,15 @@
 # To stay relevant, we will be working off the 1 October 2024 rates of PRSI.
 
 
-def prsi_rates(prsi_class:chr,income:float) ->float: # TODO: complete function
+def prsi_rates(prsi_class: chr, income: float) -> float: # TODO: complete function
     match prsi_class:
         case 'a':
-            pass
+            return (income - (352.0*52)) *.041 if income > (352.0*52) else 0.0
+        case 'b':
+            if income <= (352.0*52): return 0.0
+        case 's':
+            return income*.041
         case '_':
-            raise ValueError("Invalid prsi_class")
+            return 0.0
+
+
